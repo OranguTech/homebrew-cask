653217c92e022264f721298d1724b9e6296a6848
Pontifex Tech <matthew@pontifex-tech.com>
2026-09-06 21:22:11 -0700

    Initial Basic

---
+++ Casks/a/avaraline.avara.rb
@@ -0,0 +1,12 @@
+cask 'avaraline.avara' do
+  version '0.7.0-nightly.20260901-4af738'
+  sha256 'c18f302fd41c74962b2a5d60453b3b56f8359309e17c0c2cc3e2b8b826becd8f'
+
+  url 'https://github.com/avaraline/Avara/releases/download/0.7.0-nightly.20201230/Avara-macOS-Nightly-20260901-4af738.zip'
+  name 'Avara'
+  desc 'Modern cross-platform SourcePort of the classic Mac multiplayer game.'
+  homepage 'https://github.com/avaraline/Avara'
+
+  app 'Avara.app'
+
+end
